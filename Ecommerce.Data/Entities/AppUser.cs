5b283fcf9d84e14de42900727c2781ee92b4c49d
Hieu <quochieuu@gmail.com>
2022-03-14 22:53:27 +0700

    Add project files.

---
+++ Ecommerce.Data/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Ecommerce.Data.Entities
+{
+    public class AppUser : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string UrlAvatar { get; set; }
+    }
+}
---
+++ Ecommerce.Data/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.Data.Entities
+{
+    public class Category
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Slug { set; get; }
+        public int? ParentId { set; get; }
+
+        public bool Status { set; get; }
+        public List<Product> Products { get; set; }
+    }
+}
