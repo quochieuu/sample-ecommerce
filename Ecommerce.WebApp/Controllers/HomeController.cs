fcba8f6635db4b79702b9601eb55220a0da59081
Hieu <quochieuu@gmail.com>
2022-03-15 23:28:49 +0700

    Add list and detail product

--- Ecommerce.WebApp/Controllers/HomeController.cs
+++ Ecommerce.WebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Ecommerce.WebApp.Models;
+﻿using Ecommerce.Data.DataContext;
+using Ecommerce.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,14 +8,17 @@ namespace Ecommerce.WebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            ViewBag.ListProducts = _context.Products.OrderByDescending(c => c.Id).ToList();
             return View();
         }
 
---
+++ Ecommerce.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.Data.DataContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.WebApp.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        private readonly DataDbContext _context;
+
+        public ProductController(DataDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.ListCategories = _context.Categories.OrderByDescending(c => c.Id).ToList();  
+            var item = _context.Products.OrderByDescending(p => p.Id).ToList();
+            return View(item);
+        }
+
+        [Route("{slug}")]
+        public async Task<IActionResult> Detail(string slug)
+        {
+            var item = await _context.Products.Where(p => p.Slug == slug).FirstOrDefaultAsync();
+            return View(item);
+        }
+    }
+}
