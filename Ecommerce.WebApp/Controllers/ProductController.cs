96ec3a84bcfd0d2e8d872243b73f1e93c74a855c
Hieu <quochieuu@gmail.com>
2022-03-17 22:38:12 +0700

    Add product filter

--- Ecommerce.WebApp/Controllers/ProductController.cs
+++ Ecommerce.WebApp/Controllers/ProductController.cs
@@ -15,11 +15,45 @@ namespace Ecommerce.WebApp.Controllers
         }
 
         [Route("")]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string catelog, string q)
         {
-            ViewBag.ListCategories = _context.Categories.OrderByDescending(c => c.Id).ToList();  
-            var item = _context.Products.OrderByDescending(p => p.Id).ToList();
-            return View(item);
+            ViewBag.ListCategories = _context.Categories.OrderByDescending(c => c.Id).ToList();
+            ViewBag.q = q;
+
+            var items = await _context.Products.OrderByDescending(p => p.Id).ToListAsync();
+            
+            if(catelog != null && q != null)
+            {
+                items = await (from c in _context.Categories
+                               join pr in _context.Products on c.Id equals pr.CategoryId
+                               orderby pr.Id descending
+                               where c.Slug == catelog && pr.Name.Contains(q)
+                               select pr).ToListAsync();
+                return View(items);
+
+            } else
+            {
+                if (catelog != null)
+                {
+                    ViewBag.Category = _context.Categories.FirstOrDefault(c => c.Slug == catelog);
+
+                    items = await (from c in _context.Categories
+                                   join pr in _context.Products on c.Id equals pr.CategoryId
+                                   orderby pr.Id descending
+                                   where c.Slug == catelog
+                                   select pr).ToListAsync();
+                    return View(items);
+                }
+
+                if (q != null)
+                {
+                    items = _context.Products.Where(pr => pr.Name.Contains(q)).OrderByDescending(p => p.Id).ToList();
+                    return View(items);
+                }
+            }
+            
+            items = await _context.Products.OrderByDescending(p => p.Id).ToListAsync();
+           return View(items);
         }
 
         [Route("{slug}")]
@@ -28,5 +62,6 @@ namespace Ecommerce.WebApp.Controllers
             var item = await _context.Products.Where(p => p.Slug == slug).FirstOrDefaultAsync();
             return View(item);
         }
+
     }
 }
