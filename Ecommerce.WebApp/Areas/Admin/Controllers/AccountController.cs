10301053bcebbcd385d5eee9b6ae9e0be88ec83e
Hieu <quochieuu@gmail.com>
2022-03-17 18:43:55 +0700

    Admin order management

---
+++ Ecommerce.WebApp/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using Ecommerce.Data.DataContext;
+using Ecommerce.Data.Entities;
+using Ecommerce.Data.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin/auth")]
+    [Route("admin")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Email,
+                    model.Password,
+                    model.RememberMe,
+                    false);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("index", "home");
+                    }
+                }
+
+            }
+            return View(model);
+        }
+
+        [Route("logout")]
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
+        }
+
+        // This method added for role tutorial
+        [HttpGet]
+        [Route("access-denied")]
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
